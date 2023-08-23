f8b4c013d030180054f6e54bd4fcc0ec15e2d0dd
Nayem55 <nayem.sayed.web@gmail.com>
2023-08-24 01:13:08 +0600

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <div>
       <h1>Pamper Me</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa harum totam, reiciendis qui, possimus, corporis adipisci labore numquam nemo accusantium ea perferendis ipsum tempora aspernatur? Architecto recusandae qui a. Nulla laboriosam tempora doloremque eius cum cupiditate blanditiis. Ea quo sint ipsum nobis reprehenderit saepe esse natus, vel blanditiis aperiam officia optio, odio aliquam! Nulla nostrum incidunt beatae officiis natus architecto et ad iusto vel iste accusamus recusandae, inventore, minus, perferendis amet? Repellendus delectus dignissimos ipsa. Provident rem alias culpa consequatur exercitationem non eligendi officia voluptatibus? Amet vitae dolore labore fugit molestiae quae sunt fuga illo natus earum ipsum, voluptatem eius.</p>
     </div>
   )
 }
