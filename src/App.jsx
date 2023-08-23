de77136225311f60be68fb2e742ac0fe9bd3f96b
Nayem55 <nayem.sayed.web@gmail.com>
2023-08-24 00:51:30 +0600

    initiated collabortion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
-    <div></div>
+    <div>
+      <h1>Pamper Me</h1>
+    </div>
   )
 }
 
