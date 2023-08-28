b2f0ea24a5f4b19945c248324948d7eeb63dd6ef
rudra91199 <rskrrudra9111999@gmail.com>
2023-08-28 21:31:43 +0600

    new Component created

---
+++ src/Components/HomeServices/HomeServices.jsx
@@ -0,0 +1,10 @@
+
+const HomeServices = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HomeServices;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "../../Components/Banner/Banner";
+import HomeServices from './../../Components/HomeServices/HomeServices';
+import ChooseUs from './../../Components/Choose Us/ChooseUs';
 
 const Home = () => {
     return (
         <div>
-        <Banner></Banner>
+            <Banner></Banner>
+            <HomeServices/>
+            <ChooseUs/>
         </div>
     );
 };
