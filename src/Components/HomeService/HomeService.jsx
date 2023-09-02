57dbc7467aab800cae410a95b2923042ae11b456
Nayem55 <nayem.sayed.web@gmail.com>
2023-09-03 00:48:01 +0600

    created home services section

---
+++ src/Components/HomeService/HomeService.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import "./HomeService.css";
+
+const HomeService = ({ service }) => {
+  return (
+    <div className="serviceCard">
+      <p className="service-title">{service?.title}</p>
+      <img
+        src={service.img}
+        alt=""
+      />
+      <p className="service-category">{service.category.toUpperCase()}</p>
+      <p className="service-desc">{service.shortDescription}</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "20px",
+          fontFamily: "Arial, Helvetica, sans-serif",
+          position: "absolute",
+          bottom: "10px",
+        }}
+      >
+        <p className="service-time">{service.time} min</p>
+        <p className="service-price">TK. {service.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomeService;
--- src/Components/HomeServices/HomeServices.jsx
+++ src/Components/HomeServices/HomeServices.jsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
+import "./HomeServices.css";
+import HomeService from "../HomeService/HomeService";
+import { Link } from "react-router-dom";
 
 const HomeServices = () => {
-    return (
-        <div>
-            <h2>POPULAR SERVICES</h2>
-            <div className="home-services-container">
-                
-
-            </div>
-        </div>
-    );
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch(`service.json`)
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div className="services">
+        <h2 style={{
+          textAlign:"center",
+          fontSize:"40px"
+        }}>Popular Services</h2>
+      <div className="home-services-container">
+        {services.slice(0,3).map((service) => 
+          <HomeService key={service?.name} service={service}> </HomeService>
+        )}
+      </div>
+      <Link>View All Services</Link>
+    </div>
+  );
 };
 
 export default HomeServices;
