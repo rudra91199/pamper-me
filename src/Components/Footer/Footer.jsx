d3c10b808b57e6b056abbbd5c275ea5feccbd546
rudra91199 <rskrrudra9111999@gmail.com>
2023-09-11 22:36:25 +0600

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,59 +3,55 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className={`footer ${location.pathname=='/login'? "d-none":""}`}>
+    <div className={`footer ${location.pathname == "/login" ? "d-none" : ""}`}>
       <div className="footer-background">
-      <footer>
-        <div className="footer-container">
-          <div className="link-container">
-            <span className="link-title">GET TO KNOW US</span>
-            <Link>Terms of Service</Link>
-            <Link>Privacy Policy</Link>
-            <Link>Refund policy</Link>
-            <Link className=" mt-8">Shipping Policy</Link>
-            <Link>About Us</Link>
+        <footer>
+          <div className="footer-container">
+            <div className="link-container">
+              <span className="link-title">GET TO KNOW US</span>
+              <Link>Terms of Service</Link>
+              <Link>Privacy Policy</Link>
+              <Link>Refund policy</Link>
+              <Link className=" mt-8">Shipping Policy</Link>
+              <Link>About Us</Link>
+            </div>
+            <div className="link-container">
+              <span className="link-title">CATEGORIES</span>
+              <Link>Make up</Link>
+              <Link>Facial</Link>
+              <Link>Hair treatment</Link>
+              <Link>Body massage</Link>
+              <Link>Waxing</Link>
+              <Link>Baby care</Link>
+            </div>
+            <div className="link-container">
+              <span className="link-title">PACKAGES</span>
+              <Link>Pre Bridal Care</Link>
+              <Link>Bridal Complete Makeover</Link>
+              <Link>Luxe Beauty Complete Kit</Link>
+              <Link>Enchanted Canvas Makeup Set</Link>
+              <Link>Glamour Glow Essentials</Link>
+            </div>
+            <div className="link-container">
+              <span className="link-title">ABOUT US</span>
+              <Link>Our Story</Link>
+              <Link>Meet the Team</Link>
+              <Link>Mission and Vision</Link>
+              <Link>Awards and Recognition</Link>
+            </div>
+            <div className="link-container">
+              <span className="link-title">CONTACT</span>
+              <Link>Contact No. : 01319717565</Link>
+              <Link className=" ">Email - pamperme@gmail.com</Link>
+              <Link style={{ marginTop: "20px" }}>
+                Show some love on Social Media
+              </Link>
+            </div>
           </div>
-          <div className="link-container">
-            <span className="link-title">CATEGORIES</span>
-            <Link>Make up</Link>
-            <Link>Facial</Link>
-            <Link>Hair treatment</Link>
-            <Link>Body massage</Link>
-            <Link>Waxing</Link>
-            <Link>Baby care</Link>
-          </div>
-          <div className="link-container">
-            <span className="link-title">PACKAGES</span>
-            <Link>Pre Bridal Care</Link>
-            <Link>Bridal Complete Makeover</Link>
-            <Link>Luxe Beauty Complete Kit</Link>
-            <Link>Enchanted Canvas Makeup Set</Link>
-            <Link>Glamour Glow Essentials</Link>
-          </div>
-          <div className="link-container">
-            <span className="link-title">ABOUT US</span>
-            <Link>Our Story</Link>
-            <Link>Meet the Team</Link>
-            <Link>Mission and Vision</Link>
-            <Link>Awards and Recognition</Link>
-          </div>
-          <div className="link-container">
-            <span className="link-title">CONTACT</span>
-            <Link>
-              Contact No. : 01319717565
-            </Link>
-            <Link className=" ">
-              Email - pamperme@gmail.com
-            </Link>
-            <Link style={{marginTop:"20px"}}>
-              Show some love on Social Media
-            </Link>
-          </div>
+        </footer>
+        <div className="footer-end">
+          <p>Copyright © 2023 - All right reserved by pamper me services Ltd</p>
         </div>
-      </footer>
-      <div className="footer-end">
-        <p>Copyright © 2023 - All right reserved by pamper me services Ltd</p>
-      </div>
       </div>
     </div>
   );
