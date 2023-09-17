3420f34bb0dd4f30aa8e499b33a135273be429a5
rudra91199 <rskrrudra9111999@gmail.com>
2023-09-18 02:23:58 +0600

    sign up done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,18 @@ import HomeServices from './../../Components/HomeServices/HomeServices';
 import ChooseUs from './../../Components/Choose Us/ChooseUs';
 import './Home.css'
 import PopularProducts from "../../Components/PopularProducts/PopularProducts";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Home = () => {
+    const[user]= useAuthState(auth)
+    console.log(user);
     return (
-    <div className="home">
+        <div className="home">
             <Banner></Banner>
-            <HomeServices/>
-            <PopularProducts/>
-            <ChooseUs/>
+            <HomeServices />
+            <PopularProducts />
+            <ChooseUs />
         </div>
     );
 };
