35107fb4950cc9e5997390d1b0dbf98581040500
rudra91199 <rskrrudra9111999@gmail.com>
2023-09-17 13:23:17 +0600

    checkbox updated

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -18,10 +18,15 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import { EffectFade, Pagination, Autoplay } from "swiper/modules";
+import { useState } from "react";
 
 
 const Login = () => {
+  const [login, setLogin] = useState(true);
+  console.log(login)
   const navigate = useNavigate();
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,7 +39,7 @@ const Login = () => {
 
   return (
     <div className="authentication-page">
-         <svg onClick={()=>navigate("/")} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 320 512"><path fill="#e32085" d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+      <svg onClick={() => navigate("/")} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#e32085" d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" /></svg>
 
       <div className="authentication-container">
         {/**  TEXT LOGO  */}
@@ -43,8 +48,18 @@ const Login = () => {
             <img src={faceLogo} alt="" />
           </Link>
           <div className="form-container">
-            <h2>WELCOME <span>BACK!</span></h2>
-            <p>Login to get full access of our website</p>
+            {
+              login ?
+                <div>
+                  <h2>WELCOME <span>BACK!</span></h2>
+                  <p>Login to get full access of our website</p>
+                </div>
+                :
+                <div>
+                  <h2>GET STARTED <span>NOW!</span></h2>
+                  <p>Enter your credentials to access your account.</p>
+                </div>
+            }
 
             <button>
               <i className="fa-brands fa-google"></i>
@@ -60,6 +75,22 @@ const Login = () => {
 
             <form onSubmit={handleSubmit} className="form">
 
+              {
+                login ||
+                <div className="floating-label">
+                  <input type="text" id="name" required placeholder="" />
+                  <label htmlFor="name">Name</label>
+                </div>
+
+              }
+              {
+                login ||
+                <div className="floating-label">
+                  <input type="number" id="phone" required placeholder="" />
+                  <label htmlFor="phone">Phone</label>
+                </div>
+
+              }
               <div className="floating-label">
                 <input type="email" id="email" required placeholder="" />
                 <label htmlFor="email">Email</label>
@@ -69,18 +100,29 @@ const Login = () => {
                 <input type="password" id="pass" required placeholder="" />
                 <label htmlFor="pass">Password</label>
               </div>
-              <div className="remember-me-section">
-                <label htmlFor="remember" className="remember-me-label">
-                  <input type="checkbox" name="" id="remember" />
-                  Remember Me
-                </label>
-                <p className="forgot-pass">Forgot Password?</p>
-              </div>
 
+              {login &&
+                <div className="remember-me-section">
+                  <div>
+                    <input type="checkbox" name="" id="remember" />
+                    <label htmlFor="remember" className="remember-me-label">
+                      Remember Me
+                    </label>
+
+                  </div>
+                  <p className="forgot-pass">Forgot Password?</p>
+                </div>
+              }
               <button type="submit">Login</button>
             </form>
           </div>
         </div>
+        {
+          login ?
+            <p>Don't have an account? <span onClick={() => setLogin(false)}>Sign Up!</span></p>
+            :
+            <p>Already have an account? <span onClick={() => setLogin(true)}>Log In!</span></p>
+        }
       </div>
 
       <div className="authentication-slider">
