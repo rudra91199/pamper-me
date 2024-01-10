9714a8ecd021adf1af554d640364a085124ab2b7
Nayem55 <nayem.sayed.web@gmail.com>
2024-01-10 23:36:25 +0600

    working on services route

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -1,9 +1,28 @@
 import "./Service.css"
-const Service = () => {
+const Service = ({service}) => {
     return (
-        <div className="service-container">
-            
+        <div className="serviceCard">
+        <p className="service-title">{service?.title}</p>
+        <img
+          src={service.img}
+          alt=""
+        />
+        <p className="service-category">{service.category.toUpperCase()}</p>
+        {/* <p className="service-desc">{service.shortDescription}</p> */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "20px",
+            fontFamily: "Arial, Helvetica, sans-serif",
+            position: "absolute",
+            bottom: "10px",
+          }}
+        >
+          <p className="service-time">{service.duration}</p>
+          <p className="service-price">TK. {service.price}</p>
         </div>
+      </div>
     );
 };
 
