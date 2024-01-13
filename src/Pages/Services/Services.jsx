86307ffc7a21fd5d36c1b42c84cf80eaf6d6a5a6
Nayem55 <nayem.sayed.web@gmail.com>
2024-01-13 14:09:40 +0600

    ..

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -23,17 +23,20 @@ const Services = () => {
         selectedTab={selectedTab}
         setSelectedTab={setSelectedTab}
       ></ServicesTab>
-      <div style={{ display: "flex" }}>
+      <div style={{ display: "grid", gridTemplateColumns: "1fr 3fr", width:"100%" }}>
         <div className="filter-container">
           <div className="filterCategory">
             <p>Filter by category</p>
-            <button>category1</button>
-            <button>category2</button>
-            <button>category3</button>
-            <button>category4</button>
-            <button>category5</button>
-            <button>category6</button>
-            <button>category7</button>
+            <div className="hr"></div>
+            <button>Skin care</button>
+            <div className="hr"></div>
+            <button>Hair care</button>
+            <div className="hr"></div>
+            <button>Makeover</button>
+            <div className="hr"></div>
+            <button>Body polish</button>
+            <div className="hr"></div>
+            <button>Massage</button>
           </div>
           <div className="filterPrice">
             <p>Filter by price</p>
