0c7842981b3eb2b24e9235058f8dd97ed2787c6b
rudra91199 <rskrrudra9111999@gmail.com>
2024-01-20 20:44:04 +0600

    footer category

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,26 +1,41 @@
 import "./Footer.css";
 import { Link } from "react-router-dom";
-import logo from "../../assets/Images/Logo/logo.png";
+import logo from "../../assets/Images/Logo/FaceLogo-01.png";
+import serviceIcon from "../../assets/Images/FooterIcons/cosmetics.png";
+import contactIcon from "../../assets/Images/FooterIcons/contact.png";
+import { useContext } from "react";
+import { Context } from "../../Providers/PamperContext";
 
 const Footer = () => {
+  const {services}= useContext(Context)
   const year = new Date().getFullYear();
+
+  const filteredCategory = services.filter((service))
   return (
     <div className={`footer ${location.pathname == "/login" ? "d-none" : ""}`}>
       <footer className="footer-container">
         <div className="footer-section1">
-          <div>
-            <img src={logo} className="footerLogo" alt="" />
-            <p style={{ fontWeight: "bold" }}>One stop beauty service</p>
-            <p style={{ marginTop: "40px" }}>
+          <img src={logo} className="footerLogo" alt="" />
+          <h3>One stop beauty solution</h3>
+          <div className="footer-logo-content">
+            <p>
               In vitae nisi aliquam, scelerisque leo a, volu- tpat sem. Vivamus
               rutrum dui fermentum eros hendrerit, id lobortis leo volutpat.
             </p>
+            <i className="fa-brands fa-pinterest"></i>
+            <i className="fa-brands fa-facebook-f"></i>
+            <i className="fa-brands fa-twitter"></i>
+            <i className="fa-brands fa-dribbble"></i>
+            <i className="fa-brands fa-behance"></i>
+            <i className="fa-brands fa-linkedin-in"></i>
           </div>
         </div>
+
         <div className="footer-section2">
           <div>
-            <span className="link-title">OUR SERVICES</span>
-            <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
+            <img src={serviceIcon} alt="" />
+            <p className="link-title">OUR SERVICES</p>
+            <div style={{ display: "flex", gap: "50px", marginTop: "20px" }}>
               <div style={{ display: "flex", flexDirection: "column" }}>
                 <Link>Make up</Link>
                 <Link>Facial</Link>
@@ -31,28 +46,38 @@ const Footer = () => {
               </div>
               <div style={{ display: "flex", flexDirection: "column" }}>
                 <Link>Pre Bridal Care</Link>
-                <Link>Bridal Complete Makeover</Link>
-                <Link>Luxe Beauty Complete Kit</Link>
-                <Link>Enchanted Canvas Makeup Set</Link>
-                <Link>Glamour Glow Essentials</Link>
+                <Link>Bridal Makeover</Link>
+                <Link>Beauty Complete </Link>
+                <Link>Canvas Makeup </Link>
+                <Link>Glamour Glow</Link>
+                <Link>Hair Dressing</Link>
               </div>
             </div>
           </div>
         </div>
+
         <div className="footer-section3">
           <div>
-            <span className="link-title">CONTACT US</span>
+            <img src={contactIcon} className="footerLogo" alt="" />
+            <p className="link-title">CONTACT US</p>
             <form>
               <div>
-              <input type="text"/>
-              <input type="text"/>
+                <input type="text" />
+                <select name="interests" id="">
+                  {
+                    services.map((service)=> (
+                      
+                      <option key={service._id} value="">{service.category}</option>
+                    ))
+                  }
+                </select>
               </div>
               <div>
-              <input type="text"/>
-              <input type="text"/>
+                <input type="text" />
+                <input type="text" />
               </div>
-              <input style={{width:"55%"}} type="text"/>
-              <input className="submitBtn" type="submit" value="Submit"/>
+              <input style={{ width: "100%" }} type="text" />
+              <input className="submitBtn" type="submit" value="Submit" />
             </form>
           </div>
         </div>
