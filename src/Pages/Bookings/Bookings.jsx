bdba427aaec9c76396852942e9c232afbcec42b2
Nayem55 <nayem.sayed.web@gmail.com>
2024-01-22 17:24:48 +0600

    working on services

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -1,8 +1,9 @@
 import './Bookings.css'
 const Bookings = () => {
     return (
-        <div>
-            
+        <div className='bookings'>
+            <input type='number' placeholder='Please enter your phone number'/>
+            <button className='searchBookingBtn'>Search Bookings</button>
         </div>
     );
 };
