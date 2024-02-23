113cb01febd98dcecffbbb2f939cfd1d7af00f57
Nayem55 <nayem.sayed.web@gmail.com>
2024-02-23 20:39:45 +0600

    ..

--- src/Components/ServicesTab/ServicesTab.jsx
+++ src/Components/ServicesTab/ServicesTab.jsx
@@ -2,25 +2,33 @@ import { useLocation, useNavigate, useParams } from "react-router-dom";
 import "./ServicesTab.css";
 import { useEffect } from "react";
 
-const ServicesTab = ({services,selectedTab,setSelectedTab,name}) => {
-    const uniqueCategories = [...new Set(services?.map(service => service.category))];
-    const navigate = useNavigate();
-    const location =useLocation();
-    const {category}= useParams();
+const ServicesTab = ({ services, selectedTab, setSelectedTab, name }) => {
+  const uniqueCategories = [
+    ...new Set(services?.map((service) => service.category)),
+  ];
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { category } = useParams();
 
-    useEffect(()=>{
-        setSelectedTab(category)
-    },[category])
-    return (
-        <div className="servicesTab">
-            {uniqueCategories.map(uniqueTab => <button key={uniqueTab} 
-            onClick={()=>{
-                setSelectedTab(uniqueTab)
-                name!='services' && navigate(`/shop/${uniqueTab}`)
-        }} 
-            className={`${selectedTab==uniqueTab?"selected":""}`}>{uniqueTab.toUpperCase()}</button>)}
-        </div>
-    );
+  useEffect(() => {
+    setSelectedTab(category);
+  }, [category]);
+  return (
+    <div className="servicesTab">
+      {uniqueCategories.map((uniqueTab) => (
+        <button
+          key={uniqueTab}
+          onClick={() => {
+            setSelectedTab(uniqueTab);
+            name != "services" && navigate(`/shop/${uniqueTab}`);
+          }}
+          className={`${selectedTab == uniqueTab ? "selected" : ""}`}
+        >
+          {uniqueTab?.toUpperCase()}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default ServicesTab;
