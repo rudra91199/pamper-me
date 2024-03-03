c6a70a0b83c2a6093e19a315191b8a811b39027e
rudra91199 <rskrrudra9111999@gmail.com>
2024-03-04 01:40:02 +0600

    additional info tab

--- src/Components/ProductDetails/AdditionalInformation.jsx
+++ src/Components/ProductDetails/AdditionalInformation.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react';
-import './AdditionalInformation.css';
+import { useState } from "react";
+import "./AdditionalInformation.css";
 
 const AdditionalInformation = () => {
-    const [selectedTab, setselectedTab] = useState(1);
-    return (
-        <div className='additional-info'>
-            <div className="additional-info-tabs">
-                <button className="tab">Description</button>
-                <button className="tab">How to use</button>
-                <button className="tab">Reviews</button>
-            </div>
-        </div>
-    )
-}
+  const [selectedTab, setselectedTab] = useState(1);
+  return (
+    <div className="additional-info">
+      <div className="additional-info-tabs">
+        <button
+          onClick={() => setselectedTab(1)}
+          className={`tab ${selectedTab === 1 && "tab-active"}`}
+        >
+          Description
+        </button>
+        <button
+          onClick={() => setselectedTab(2)}
+          className={`tab ${selectedTab === 2 && "tab-active"}`}
+        >
+          How to use
+        </button>
+        <button
+          onClick={() => setselectedTab(3)}
+          className={`tab ${selectedTab === 3 && "tab-active"}`}
+        >
+          Reviews
+        </button>
+      </div>
+      <hr />
+    </div>
+  );
+};
 
-export default AdditionalInformation
+export default AdditionalInformation;
