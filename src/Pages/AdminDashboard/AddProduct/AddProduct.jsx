17f33f9c7201b90551ea3ed29cb931b01aa808bb
Nayem55 <nayem.sayed.web@gmail.com>
2024-05-12 18:41:21 +0600

    working on admin panel

--- src/Pages/AdminDashboard/AddProduct/AddProduct.jsx
+++ src/Pages/AdminDashboard/AddProduct/AddProduct.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import './AddProduct.css'
+import React, { useContext } from "react";
+import "./AddProduct.css";
+import { Context } from "../../../Providers/PamperContext";
 
 const AddProduct = () => {
-    return (
-        <div className='addProduct'>
-            <h1>Add Product</h1>
+  const { products } = useContext(Context);
+  console.log(products.products[0]);
+
+  return (
+    <div className="addProduct">
+      <h1>Add Product</h1>
+
+      <div className="add-form">
+        <input type="text" placeholder="Product Name" />
+        <input type="text" placeholder="Product Slug" />
+        <input type="number" placeholder="Product Price" />
+        <textarea rows={10} placeholder="Product Description" />
+        <textarea rows={10} placeholder="Product Short Description" />
+        <textarea rows={10} placeholder="Product Meta Description" />
+        <input type="number" placeholder="Sale Price" />
+        <input type="text" placeholder="On Sale?" />
+        <input type="text" placeholder="Category" />
+        <input type="text" placeholder="Subcategory" />
+        <input type="number" placeholder="Stock Quantity" />
+        <input type="text" placeholder="Tags" />
+        <input type="text" placeholder="Variation" />
+        <input type="text" placeholder="Attributes" />
+      </div>
+      <div className="add-image">
+        <p>Product Image</p>
+        <input type="file"/>
+        <p>Preview</p>
+        <div className="image-preview">
+
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddProduct;
